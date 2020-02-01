756aa857f9d624afaa3ed009389192b4695ec6b0
OmegaXxX <35782519+lavalxavier@users.noreply.github.com>
2020-02-01 00:45:20 -0600

    Create INFO.bat

---
+++ List/INFO.bat
@@ -0,0 +1,66 @@
+@echo off
+:INF
+cls
+echo Type your name to find your INFO
+set /p name=
+if %name%== Sosigs goto SosigsI
+if %name%== GpXavier goto GpXavierI
+if %name%== Sphinx0023 goto Sphinx0023I
+if %name%== Wesisbest goto WesisbestI
+if %name%== Littletoetag goto LittletoetagI
+if %name%== Santamoose123 goto Santamoose123I
+ if NOT %name%== Sosigs goto INF
+ if NOT %name%== GpXavier goto INF
+ if NOT %name%== Sphinx0023 goto INF
+ if NOT %name%== Wesisbest goto INF
+ if NOT %name%== Littletoetag goto INF
+ if NOT %name%== Santamoose123 goto INF
+
+:SosigsI
+cls
+echo Minecraft name Sosigs
+echo Server Owner Yes
+echo In the-crew Yes
+echo is Friend Yes
+echo More is coming
+goto INF
+:GpXavierI
+cls
+echo Minecraft name GpXavier
+echo Server Owner NO
+echo Is Devloper Yes
+echo In the-crew Yes
+echo is Friend Yes
+echo More is coming
+goto INF
+:Sphinx0023I
+cls
+echo Minecraft name Sphinx0023
+echo Server Owner  No
+echo In the-crew Yes
+echo is Friend Yes
+echo More is coming
+goto INF
+:WesisbestI
+cls
+echo Minecraft name Wesisbest
+echo Server Owner No
+echo In the-crew Yes
+echo is Friend Yes
+echo More is coming
+goto INF
+:LittletoetagI
+cls
+echo Minecraft name Littletoetag
+echo Server Owner No
+echo In the-crew Yes
+echo is Friend Yes
+echo More is coming
+goto INF
+:Santamoose123I
+echo Minecraft name Santamoose123
+echo Server Owner No
+echo In the-crew Yes
+echo is Friend Yes
+echo More is coming
+goto INF
