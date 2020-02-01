4d32cc12a548db9151ad77020ef6021badc1ad1a
OmegaXxX <35782519+lavalxavier@users.noreply.github.com>
2020-02-01 00:49:43 -0600

    Small update 9-10
    Small Update

--- List/INFO.bat
+++ List/INFO.bat
@@ -1,6 +1,5 @@
 @echo off
- echo INFO
-:INFO
+:INF
 cls
 echo Type your name to find your INFO
 set /p name=
@@ -10,12 +9,12 @@ if %name%== Sphinx0023 goto Sphinx0023I
 if %name%== Wesisbest goto WesisbestI
 if %name%== Littletoetag goto LittletoetagI
 if %name%== Santamoose123 goto Santamoose123I
- if NOT %name%== Sosigs goto INFO
- if NOT %name%== GpXavier goto INFO
- if NOT %name%== Sphinx0023 goto INFO
- if NOT %name%== Wesisbest goto INFO
- if NOT %name%== Littletoetag goto INFO
- if NOT %name%== Santamoose123 goto INFO
+ if NOT %name%== Sosigs goto INF
+ if NOT %name%== GpXavier goto INF
+ if NOT %name%== Sphinx0023 goto INF
+ if NOT %name%== Wesisbest goto INF
+ if NOT %name%== Littletoetag goto INF
+ if NOT %name%== Santamoose123 goto INF
 
 :SosigsI
 cls
@@ -24,7 +23,10 @@ echo Server Owner Yes
 echo In the-crew Yes
 echo is Friend Yes
 echo More is coming
-goto INFO
+echo would you like to go back to The-Crew-Script type Y/N
+set /p ans=
+if %ans%== Y goto Cal
+if %ans%== N goto INF
 :GpXavierI
 cls
 echo Minecraft name GpXavier
@@ -33,7 +35,10 @@ echo Is Devloper Yes
 echo In the-crew Yes
 echo is Friend Yes
 echo More is coming
-goto INFO
+echo would you like to go back to The-Crew-Script type Y/N
+set /p ans=
+if %ans%== Y goto Cal
+if %ans%== N goto INF
 :Sphinx0023I
 cls
 echo Minecraft name Sphinx0023
@@ -41,7 +46,10 @@ echo Server Owner  No
 echo In the-crew Yes
 echo is Friend Yes
 echo More is coming
-goto INFO
+echo would you like to go back to The-Crew-Script type Y/N
+set /p ans=
+if %ans%== Y goto Cal
+if %ans%== N goto INF
 :WesisbestI
 cls
 echo Minecraft name Wesisbest
@@ -49,7 +57,10 @@ echo Server Owner No
 echo In the-crew Yes
 echo is Friend Yes
 echo More is coming
-goto INFO
+echo would you like to go back to The-Crew-Script type Y/N
+set /p ans=
+if %ans%== Y goto Cal
+if %ans%== N goto INF
 :LittletoetagI
 cls
 echo Minecraft name Littletoetag
@@ -57,11 +68,21 @@ echo Server Owner No
 echo In the-crew Yes
 echo is Friend Yes
 echo More is coming
-goto INFO
+echo would you like to go back to The-Crew-Script type Y/N
+set /p ans=
+if %ans%== Y goto Cal
+if %ans%== N goto INF
 :Santamoose123I
 echo Minecraft name Santamoose123
 echo Server Owner No
 echo In the-crew Yes
 echo is Friend Yes
 echo More is coming
-goto INFO
+echo would you like to go back to The-Crew-Script type Y/N
+set /p ans=
+if %ans%== Y goto Cal
+if %ans%== N goto INF
+
+:Cal
+cls
+call C:\The-Crew-Script-beta\The-Crew\The-Crew.bat
